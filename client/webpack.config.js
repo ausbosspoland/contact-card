65639d2bf86976aa80aef32bf0861877ae3cee3a
ausbosspoland <austinjpoland311@gmail.com>
2022-09-18 18:50:48 -0500

    configures manifest.json for application, now to installability

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
@@ -11,13 +12,35 @@ module.exports = {
   },
   
   plugins: [
-    new InjectManifest({
-      swSrc: './src/sw.js',
-      swDest: 'service-worker.js',
-    }),
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'Webpack Plugin',
+    }),
+    new WebpackPwaManifest({
+      name: 'Contact Cards Application',
+      short_name: 'Contact Cards',
+      description: 'Keep track of contacts!',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/icon-manifest.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+        {
+          src: path.resolve('src/images/icon-manifest.png'),
+          size: '1024x1024',
+          destination: path.join('assets', 'icons'),
+          purpose: 'maskable'
+        }
+      ],
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
     }),
   ],
   module: {
